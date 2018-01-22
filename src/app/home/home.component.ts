a522645d25cf7be1b6b1105c68369b840115b5db
Nagaraja Manikandan (sv291h) <mani_ors@hotmail.com>
2018-01-22 23:03:36 +0530

    initial commit angular poc

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import { TabsModule } from 'ngx-bootstrap';
+
+@Component({
+  selector: 'home',
+  styleUrls: ['./home.component.css'],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+}
